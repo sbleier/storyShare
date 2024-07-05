434275166632691b73da6534d06c86e70ceeb317
Devorah135 <155027819+Devorah135@users.noreply.github.com>
2024-07-05 13:25:10 -0400

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,7 +1,9 @@
-<?php
+<p>Menu</p>
 
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
+<ul>
+  <li><a href="HomePage.html">Home</li>
+  <li><a href="StoryForm.html">Submit a Story</li>
+  <li><a href="stories.html">Browse stories</li>
+  <li><a href="contactus.php">Contact Us</li>
 
+</ul>
