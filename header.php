24893bf9527520724cb801969083984432836ee8
sbleier <sbleier2@student.touro.edu>
2024-07-05 11:11:44 -0500

    Add files via upload

---
+++ header.php
@@ -0,0 +1,15 @@
+<HTML><
+    <HEAD>
+        <LINK rel='stylesheet' href='styles.css' type='text/css' />
+    </HEAD>
+    
+    <BODY>
+        <P><img src='logo.png' alt='logo' /> </P>
+       
+<?php
+include 'menu.php';
+?>
+        <HR/>
+
+
+
