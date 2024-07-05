ac604b9750ac2bca92adf41d97149801a249e53e
Devorah135 <155027819+Devorah135@users.noreply.github.com>
2024-07-05 13:26:43 -0400

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,9 +1,9 @@
 <p>Menu</p>
 
 <ul>
-  <li><a href="HomePage.html">Home</li>
-  <li><a href="StoryForm.html">Submit a Story</li>
-  <li><a href="stories.html">Browse stories</li>
+  <li><a href="index.php">Home</li>
+  <li><a href="story_submit.php">Submit a Story</li>
+  <li><a href="stories.php">Browse stories</li>
   <li><a href="contactus.php">Contact Us</li>
 
 </ul>
