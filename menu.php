c39bb8b4c749c25201127aada446c14aef5f70f9
Devorah135 <155027819+Devorah135@users.noreply.github.com>
2024-07-07 12:21:27 -0400

    Update menu.php

--- menu.php
+++ menu.php
@@ -4,5 +4,5 @@
   <li class="menu-item"><a href="story_submit.php">Submit a Story</a></li>
   <li class="menu-item"><a href="stories.php">Browse stories</a></li>
   <li class="menu-item"><a href="contactus.php">Contact Us</a></li>
-  <li class="menu-item"><a href="login_page.php">Admin Login</a></li>
+  <li class="menu-item"><a href="login.php">Admin Login</a></li>
 </ul>
