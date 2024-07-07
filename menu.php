d1cd389b80cd9446daef7700c4c2b4c5f92a80b2
Devorah135 <155027819+Devorah135@users.noreply.github.com>
2024-07-07 16:18:42 +0000

    made logo a link to home page

--- menu.php
+++ menu.php
@@ -1,5 +1,5 @@
 <ul>
-  <li class="menu-item"><img src="logo.png" alt="Story Share Logo"/></li>
+  <li class="menu-item"><a href="index.php"><img src="logo.png" alt="Story Share Logo"/></a></li>
   <li class="menu-item"><a href="index.php">Home</a></li>
   <li class="menu-item"><a href="story_submit.php">Submit a Story</a></li>
   <li class="menu-item"><a href="stories.php">Browse stories</a></li>
