0b55eb7cf084225a2feb65e7e672731a777d141e
Devorah135 <155027819+Devorah135@users.noreply.github.com>
2024-07-07 16:15:01 +0000

    <br>

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
 <form method="post" action="login_response.php">
     <label for="username">USERNAME:</label><br>
     <input name="username" id="username" type="text" required />
-    
+    <br>
     <label for="password">PASSWORD:</label><br>
     <input name="passworkd" id="password" type="password" required />
 </form>
