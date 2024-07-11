22a46dc5deba25e6e002ec1974d91c0e175e04a0
Devorah135 <155027819+Devorah135@users.noreply.github.com>
2024-07-11 02:22:19 -0400

    fixed login form

--- login.php
+++ login.php
@@ -2,12 +2,15 @@
 
 <h1>Admin Login:</h1>
 
-<form method="post" action="login_response.php">
+<form method="post" action="responcePage.php">
     <label for="username">USERNAME:</label><br>
     <input name="username" id="username" type="text" required />
     <br>
     <label for="password">PASSWORD:</label><br>
     <input name="passworkd" id="password" type="password" required />
+    <br>
+    <button type="submit" value="submit">Submit</button>
 </form>
+
 
 <?php include 'footer.php'; ?>
