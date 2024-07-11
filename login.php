f394459cbcfe7ea43a114e0a34ce9e4268c34321
Devorah135 <155027819+Devorah135@users.noreply.github.com>
2024-07-11 16:01:37 -0400

    Update login.php

--- login.php
+++ login.php
@@ -2,14 +2,14 @@
 
 <h1>Admin Login:</h1>
 
-<form method="post" action="responcePage.php">
-    <label for="username">USERNAME:</label><br>
-    <input name="username" id="username" type="text" required />
+<form method="post" action="responsePage.php">
+    <label for="username">Username:</label><br>
+    <input type="text" name="username" id="username" required />
     <br>
-    <label for="password">PASSWORD:</label><br>
-    <input name="passworkd" id="password" type="password" required />
+    <label for="password">Password:</label><br>
+    <input type="password" name="passworkd" id="password" required />
     <br>
-    <button type="submit" value="submit">Submit</button>
+    <button type="submit" value="login">Submit</button>
 </form>
 
 
