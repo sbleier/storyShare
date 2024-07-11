98a768e40b639d9909a09c7a6c37b7cb32e71cd2
tkrupka12 <tkrupka3@student.touro.edu>
2024-07-11 01:52:22 -0400

    Add files via upload
    please check over and make changes as youd like

---
+++ contentPage.php
@@ -0,0 +1,15 @@
+<?php
+
+session_start();
+if (isset($_SESSION['username'])) {
+  $username = $_SESSION['username'];
+  echo "Hello, $username!";
+} else {
+  echo 'Error: You are not logged in';
+}
+
+?>
+
+<form method="post" action="responcePage.php">
+  <input type="submit" value="Submit">
+</form>
---
+++ responcePage.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+
+$valid_usernames = ['user1', 'user2', 'user3'];
+$valid_passwords = ['password1', 'password2', 'password3'];
+
+$username = isset($_POST['username']) ? trim($_POST['username']) : '';
+$password = isset($_POST['password']) ? trim($_POST['password']) : '';
+
+$valid_login = false;
+
+if (in_array($username, $valid_usernames)) {
+  $index = array_search($username, $valid_usernames);
+  if (password_verify($password, $valid_passwords[$index])) {
+    $valid_login = true;
+  }
+}
+
+$_SESSION['logged_in'] = $valid_login;
+
+if ($valid_login) {
+  setcookie('username', $username, time() + (86400 * 30), "/"); // Expires in 30 days
+}
+
+if ($valid_login) {
+  echo "Login successful! Welcome, " . $username . ".";
+  echo "<br><a href='content.php'>Go to Content Page</a>";
+} else {
+  echo "Invalid username or password.";
+}
+?>
