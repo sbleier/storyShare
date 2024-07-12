29d8196e1d8ced102847a2b837fd5263b681314e
Devorah135 <155027819+Devorah135@users.noreply.github.com>
2024-07-11 22:51:46 -0400

    Update contactus.php

--- contactus.php
+++ contactus.php
@@ -1,7 +1,15 @@
 <?php include "header.php"; ?>
 
 <h1>Contact Information: </h1>
-<p><a href="mailto:admin@storyshare.com">Email</a></p>
-<p><a href="tel:1234567890">Phone</a></p>
+
+<p>
+    <h4>Email:</h4>
+    <a href="mailto:admin@storyshare.com">admin@storyshare.com</a>
+</p>
+
+<p>
+    <h4>Phone:</h4>
+    <a href="tel:1234567890">(123) 456-7890</a>
+</p>
 
 <?php include "footer.php"; ?>
