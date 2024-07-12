1a8ae1e8344cc7e18b1afbb7d32288cfe8cc7bf7
Devorah135 <155027819+Devorah135@users.noreply.github.com>
2024-07-11 22:55:30 -0400

    Updated password verification

--- responsePage.php
+++ responsePage.php
@@ -1,6 +1,7 @@
 <?php
-include 'header.php'; 
 session_start();
+include 'header.php'; 
+
 
 $valid_usernames = ['user1', 'user2', 'user3'];
 $valid_passwords = ['password1', 'password2', 'password3'];
@@ -12,7 +13,7 @@ $valid_login = false;
 
 if (in_array($username, $valid_usernames)) {
   $index = array_search($username, $valid_usernames);
-  if (password_verify($password, $valid_passwords[$index])) {
+  if ($password === $valid_passwords[$index]) {
     $valid_login = true;
   }
 }
@@ -23,7 +24,7 @@ $_SESSION['username'] = $username;
 if ($valid_login) {
   setcookie('username', $username, time() + (86400 * 30), "/"); // Expires in 30 days
 
-  echo "Login successful! Welcome, " . $username . ".";
+  echo "Login successful! Welcome, " . htmlspecialchars($username) . "!";
   echo "<br><a href='contentPage.php'>Go to Content Page</a>";
 } 
 else {
