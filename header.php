bcd66e518b73e9295d29fa5acb390d77f783f5ac
Devorah135 <155027819+Devorah135@users.noreply.github.com>
2024-07-26 16:22:13 -0400

    Update header.php

--- header.php
+++ header.php
@@ -1,5 +1,7 @@
-<HTML>
+<HTML lang="en">
     <HEAD>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <LINK rel='stylesheet' href='styles.css' type='text/css' />
         <LINK rel='stylesheet' href='ResponsiveSlides.js-master/responsiveslides.css' type='text/css' />
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
@@ -12,22 +14,7 @@
         <header>
             <?php include 'menu.php'; ?>
         </header>  
-        
-         <ul class="rslides">
-            <li><img src="1.jpg" alt="slider image"></li>
-            <li><img src="2.png" alt="slider image"></li>
-            <li><img src="3.jpg" alt="slider image"></li>
-        </ul>
     
-        <script>
-        $(function() {
-             $(".rslides").responsiveSlides({  auto: true,          
-                    speed: 500,           
-                    timeout: 4000,         
-                    pager: false,           
-                    nav: false,                
-                })
-            });
 </script>
        
 
