50ac3bc4495403c85cdcc0f536b5361ff0cd6c03
Devorah135 <155027819+Devorah135@users.noreply.github.com>
2024-07-27 23:34:20 -0400

    Update story_submit.php

--- story_submit.php
+++ story_submit.php
@@ -9,41 +9,59 @@ $result_topics = $conn->query($sql_topics);
 $sql_positions = "SELECT * FROM positions";
 $result_positions = $conn->query($sql_positions);
 ?>
-       <form name="StorySubmission" method="post" action="results.php">
+   <form name="StorySubmission" method="post" action="results.php">
+   <div id="submission-fields">
     <!-- User information -->
     <fieldset id="user_info">
         <legend>User Information</legend>
-        <label for="fname">First Name:*</label>
-        <input type="text" id="fname" name="first_name" placeholder="First" size="8" required/>
-        <label for="lname">Last Name:*</label>
-        <input type="text" id="lname" name="last_name" placeholder="Last" size="8" required /><br>
-        <label for="email">Email:*</label>
-        <input type="email" id="email" name="email" placeholder="story@example.com" required /><br>
+        <div class="form-group">
+            <label for="fname">First Name:*</label>
+            <input type="text" id="fname" class="form-input" name="first_name" placeholder="First" size="8" required/>
+        </div>
+        
+        <div class="form-group">
+            <label for="lname">Last Name:*</label>
+            <input type="text" id="lname" class="form-input" name="last_name" placeholder="Last" size="8" required />
+        </div>
+        
+        <div class="form-group">
+            <label for="email">Email:*</label>
+            <input type="email" id="email" class="form-input" name="email" placeholder="story@example.com" required /><br>
+        </div>
+        
         <input type="checkbox" id="show_email" name="show_email" value="show_email"/>
-        <label for="show_email">Share Email with Users?</label><br> 
-        <label for="position">Position*</label><br>
-        <select name="position" id="position" required>
-            <?php
-            if ($result_positions->num_rows > 0) {
-                // Output data of each row
-                while($row = $result_positions->fetch_assoc()) {
-                    echo '<option value="' . htmlspecialchars($row["position_id"]) . '">' . htmlspecialchars($row["position_name"]) . '</option>';
+        <label for="show_email">Share Email with Users?</label><br> <br>
+        
+        <div class="form-group">
+            <label for="position">Position*</label><br>
+            <select name="position" id="position" class="form-input" required>
+                <?php
+                if ($result_positions->num_rows > 0) {
+                    // Output data of each row
+                    while($row = $result_positions->fetch_assoc()) {
+                        echo '<option value="' . htmlspecialchars($row["position_id"]) . '">' . htmlspecialchars($row["position_name"]) . '</option>';
+                    }
+                } else {
+                    echo '<option value="">No positions available</option>';
                 }
-            } else {
-                echo '<option value="">No positions available</option>';
-            }
-            ?>
-        </select>
+                ?>
+            </select>
+        </div>
     </fieldset>
     
     <!-- Story information -->
     <fieldset id="story_info"> 
         <legend>Story Information</legend>
         <div id="title-and-story">
-            <label for="title">Story Title:</label>
-            <input type="text" name="title" id="title" value="" required/><br>
-            <label for="story">Enter the story:</label><br>
-            <textarea id="story" name="story" rows="5" cols="40" placeholder="Your story here:" required></textarea><br>
+            <div class="form-group">
+                <label for="title">Story Title:</label>
+                <input type="text" class="form-input" name="title" id="title" value="" required/>
+            </div>
+            
+            <div class="form-group">
+                <label for="story">Enter the story:</label><br>
+                <textarea id="story" class="form-input" name="story" rows="5" cols="40" placeholder="Your story here:" required></textarea>
+            </div>
         </div>
         
         <!-- Topics -->
@@ -55,7 +73,7 @@ $result_positions = $conn->query($sql_positions);
                 while($row = $result_topics->fetch_assoc()) {
                      $topicId = htmlspecialchars($row["topic_id"]);
                      $topicName = htmlspecialchars($row["topic_name"]);
-                       
+                     
                     echo '<input type="checkbox"' . 'id="' . $topicName . '" name="topic[]" value="' . $topicId . '" />';
                     echo '<label for="' . $topicName . '">' . $topicName . '</label><br>';
                 }
@@ -66,8 +84,8 @@ $result_positions = $conn->query($sql_positions);
             ?>
         </div>
     </fieldset>
-    
-    <button type="submit" value="submit">Submit Story</button>
+   </div>
+    <button type="submit" value="submit" class="btn btn-primary">Submit Story</button>
 </form>
 
 <?php include "footer.php"; ?>
