e348b69ce6642dd0e3e67e5cc7fa3aa3e181a3e0
Devorah135 <155027819+Devorah135@users.noreply.github.com>
2024-07-28 12:06:48 -0400

    Update stories.php to include topics

--- stories.php
+++ stories.php
@@ -11,17 +11,42 @@ require 'dhb.Inc';
     $story_result = mysqli_query($conn, $story_query);
 
     if (mysqli_num_rows($story_result) > 0) {
+        // for each row
       while ($row = mysqli_fetch_assoc($story_result)) {
         // Access story data and display them within HTML elements
         $title = $row["title"];
         $author = $row["author"];
         $content = $row["content"];
         $is_approved = $row["is_approved"];
+        $show_email = $row["show_email"];
+        $author_email = $row["author_email"];
+        $topic_ids = json_decode($row["topic_ids"], true);
+        
+        //get an array of the topic NAMES if the array of IDs is not empty
+        if (!empty($topic_ids)) {
+            // impolde creates the array into a string seperated by commas so that the IN query works. intval makes it into integer values
+            $topics_query = "SELECT topic_name FROM topics WHERE topic_id IN (" . implode(',', array_map('intval', $topic_ids)) . ")";
+            $topics_result = mysqli_query($conn, $topics_query);
 
+            $topic_names = [];
+
+            while ($topic_row = mysqli_fetch_assoc($topics_result)) {
+                $topic_names[] = $topic_row['topic_name']; // add topic name from current row to the array
+            }
+            $topics_display = "<small class='topics'>Topics: " . implode(', ', $topic_names) . "</small></p>";
+        }
+        else {
+            $topics_display = "<small class ='topics'>No topics chosen.</small></p>";
+        }
+        
         if ($is_approved){
-            echo "<p class='story'>";
-            echo "<strong>$title - <em>$author</em></strong>";
-            echo "<br>$content</p>";
+            echo "<hr><p class='story'>";
+            echo "<h4>$title</h4>";
+            echo "<strong>Author: </strong>$author";
+                if ($show_email)
+                    echo " - <em>$author_email</em>";
+            echo "<blockquote>$content</blockquote></p>";
+            echo $topics_display;
             echo "</div>";
          } 
       
@@ -29,8 +54,9 @@ require 'dhb.Inc';
     } else {
       echo "No stories found";
     }
-    
 
     ?>
+
+<button class="btn btn-primary"><a href="contentPage.php">View unapproved stories</a></button>
 
 <?php include "footer.php"; ?>
