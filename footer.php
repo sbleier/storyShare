ec8ea1bde992683857ed77982009a0c423cd16e3
Devorah135 <155027819+Devorah135@users.noreply.github.com>
2024-07-28 22:39:17 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,23 @@
+ <hr>
 <footer>
-    <hr />
+   
 <p> &copy; 2024 - Sara Bleier, Deborah Schonkopf, Tova Krupka </p>
-<p><a href = "contactus.php">Contact Us</a></p>
+<!--<p><a href = "contactus.php">Contact Us</a></p>-->
 </footer>
+
+<script src="jquery.slimmenu.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+<script>
+    $('#navigation').slimmenu({
+        resizeWidth: '700',
+        collapserTitle: 'Main Menu',
+        animSpeed: 'medium',
+        easingEffect: null,
+        indentChildren: false,
+        childrenIndenter: '&nbsp;'
+    });
+
+</script>
+
 </body>
 </html>
