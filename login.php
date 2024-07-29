ec44c61e83c8f112435191b4678f157e7d84f67f
Devorah135 <155027819+Devorah135@users.noreply.github.com>
2024-07-28 22:46:00 -0400

    Update login.php to add div and classes

--- login.php
+++ login.php
@@ -1,16 +1,19 @@
-<?php include 'header.php'; ?>
+<?php include __DIR__ . '/header.php'; ?>
 
 <h1>Admin Login:</h1>
 
 <form method="post" action="responsePage.php">
-    <label for="username">Username:</label><br>
-    <input type="text" name="username" id="username" required />
-    <br>
-    <label for="password">Password:</label><br>
-    <input type="password" name="password" id="password" required />
-    <br>
-    <button type="submit" value="login">Submit</button>
+    <div class="form-group">
+        <label for="username">Username:</label>
+        <input type="text" name="username" id="username" class="form-input" required>
+    </div>
+    
+    <div class="form-group">
+        <label for="password">Password:</label>
+        <input type="password" name="password" id="password" class="form-input" required>
+    </div>
+    
+    <button type="submit" value="login" class="btn btn-primary">Submit</button>
 </form>
-
 
 <?php include 'footer.php'; ?>
